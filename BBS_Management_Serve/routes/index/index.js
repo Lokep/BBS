afa08d5de672cd15d4fd76bf7bf9b602eb858fab
weblixin <weblixin@163.com>
2021-03-08 18:50:28 +0800

    页面刷新问题

--- BBS_Management_Serve/routes/index/index.js
+++ BBS_Management_Serve/routes/index/index.js
@@ -7,14 +7,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/manage/*', function(req, res, next) {
-//   console.log('...get /manage/* ...');
-//   res.sendFile(path.join(__dirname, '../../dist', 'home.html'));
-// });
-
-router.get('/manage', function(req, res, next) {
-  console.log('...get /manage ...');
+router.get('/manage/*', function(req, res, next) {
+  console.log('...get /manage/* ...');
   res.sendFile(path.join(__dirname, '../../dist', 'home.html'));
 });
+
+// router.get('/manage', function(req, res, next) {
+//   console.log('...get /manage ...');
+//   res.sendFile(path.join(__dirname, '../../dist', 'home.html'));
+// });
 
 module.exports = router;
