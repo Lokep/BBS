3cb5ff8adfeffa3d4703e1b2638e90aa96c1f50d
weblixin <weblixin@163.com>
2021-03-08 16:56:27 +0800

    处理路由刷新报错问题

--- BBS_Management_Serve/routes/index/index.js
+++ BBS_Management_Serve/routes/index/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('...get / ...');
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 module.exports = router;
